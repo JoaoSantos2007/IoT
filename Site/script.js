f6929ed55e82eaa8341ff792f21b077d3232e716
João Pedro Tomaz dos Santos <80163527+JoaoSantos2007@users.noreply.github.com>
2021-07-05 20:04:28 -0300

    update

---
+++ Site/script.js
@@ -0,0 +1,24 @@
+var audio = new Audio()
+
+function reproduzir_audio() {
+  audio.src = "/Site/audios/audio.mp3"
+  audio.play()
+}
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAPcEx7NNKgLOSlsjQXkbv3QaLHLhe27m8",
+  authDomain: "app-teste-ad56f.firebaseapp.com",
+  databaseURL: "https://app-teste-ad56f-default-rtdb.firebaseio.com",
+  projectId: "app-teste-ad56f",
+  storageBucket: "app-teste-ad56f.appspot.com",
+  messagingSenderId: "622101019355",
+  appId: "1:622101019355:web:92513b0aed87fc3ab66afe",
+  measurementId: "G-84B0BBVRW2"
+};
+
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+var db = firebase.firestore();
+

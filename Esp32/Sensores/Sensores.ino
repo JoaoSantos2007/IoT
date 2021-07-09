84bdaa9c96a1c7a5d5ff0ed77dd886d0ebd7ee83
Aleksandro Santos <aleksandro.santos.sp@gmail.com>
2021-07-09 18:49:48 -0300

    update

--- Esp32/Sensores/Sensores.ino
+++ Esp32/Sensores/Sensores.ino
@@ -1,30 +1,31 @@
-int estado = 0; 
-int PIN_LUMINOSIDADE = 35;
-int PIN_PRESENCA = 26;
-/* Headers */ 
-#include <WiFi.h> /* Header para uso das funcionalidades de wi-fi do ESP32 */
-#include <PubSubClient.h>  /*  Header para uso da biblioteca PubSubClient */
+
+/* Headers */
+#include <WiFi.h>         /* Header para uso das funcionalidades de wi-fi do ESP32 */
+#include <PubSubClient.h> /*  Header para uso da biblioteca PubSubClient */
 #include <ArduinoJson.h>
 
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
 //Bibliotecas do sensor de Umidade/Temperatura
-#include <Adafruit_Sensor.h>
 #include <DHT.h>
 
+String DEVICE_ID = "1";
+int PIN_LUMINOSIDADE = 35;
+int PIN_PRESENCA = 26;
 
-String DEVICE_ID = "1";//ESP_ID
-
+unsigned long previousMillis = 0;   
+const long interval = 60000;
+bool wasPresence = false;
 
 //Definições do MQTT
 
 //Tópico MQTT para recepção de informações do broker MQTT para ESP32
 #define TOPICO_SUBSCRIBE "topico-1"
-   
+
 //Tópico MQTT para envio de informações do ESP32 para broker MQTT
-#define TOPICO_PUBLISH   "test"
-  
+#define TOPICO_PUBLISH "test"
+
 /* id mqtt (para identificação de sessão)
    IMPORTANTE: este deve ser único no broker (ou seja, 
                se um client MQTT tentar entrar com o mesmo 
@@ -32,120 +33,125 @@ String DEVICE_ID = "1";//ESP_ID
                irá fechar a conexão de um deles).
 */
 
+#define ID_MQTT "Cliente_MQTT"
 
-#define ID_MQTT  "Cliente_MQTT" 
-    
 //Variáveis e constantes globais
 
 // SSID / nome da rede WI-FI que deseja se conectar
-const char* SSID = "xiaomi_2G"; 
+const char *SSID = "xiaomi_2G";
 //Senha da rede WI-FI que deseja se conectar
-const char* PASSWORD = "JP010203"; 
+const char *PASSWORD = "JP010203";
 
-  
 //URL do broker MQTT que deseja utilizar
-const char* BROKER_MQTT = "192.168.31.45";
+const char *BROKER_MQTT = "192.168.31.45";
 //Porta do Broker MQTT
 int BROKER_PORT = 1883;
-
 
 //Variáveis e objetos globais
 WiFiClient espClient;
 PubSubClient MQTT(espClient);
 
-WiFiUDP ntpUDP; // UDP client
+WiFiUDP ntpUDP;               // UDP client
 NTPClient timeClient(ntpUDP); // NTP client
 
 //Sensor de Umidade e de Temperatura
-#define DHTPIN 27     // Pino digital conectado ao sensor DHT
-#define DHTTYPE    DHT11
+#define DHTPIN 27 // Pino digital conectado ao sensor DHT
+#define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-
-String readDHTTemperature() {
+String readDHTTemperature()
+{
   float t = dht.readTemperature();
-  if (isnan(t)) {    
+  if (isnan(t))
+  {
     Serial.println("Falha ao ler o sensor DHT!");
     return "--";
   }
-  else {
+  else
+  {
     return String(t);
   }
 }
 
-String readDHTHumidity() {
+String readDHTHumidity()
+{
   float h = dht.readHumidity();
-  if (isnan(h)) {
+  if (isnan(h))
+  {
     return "--";
   }
-  else {
+  else
+  {
     return String(h);
   }
 }
 
-String readLDRLuminosidade(){
+String readLDRLuminosidade()
+{
   float v = analogRead(PIN_LUMINOSIDADE);
-  if (isnan(v)) {
+  if (isnan(v))
+  {
     return "--";
-  } 
-  else {
+  }
+  else
+  {
     return String(v);
   }
-  
 }
 
-String readPIRPresenca(){
-  float p = digitalRead(PIN_PRESENCA);
-  if (isnan(p)) {
+bool readPIRPresenca()
+{
+  bool p = digitalRead(PIN_PRESENCA);
+  if (isnan(p))
+  {
     return "--";
-  } 
-  else {
-    return String(p);
+  }
+  else
+  {
+    return p;
   }
 }
 
-
 //Fim sensor de umidade
-  
+
 //Prototypes
 void init_serial(void);
 void init_wifi(void);
 void init_mqtt(void);
-void reconnect_wifi(void); 
+void reconnect_wifi(void);
 void verifica_conexoes_wifi_mqtt(void);
 void send_payload(void);
 void init_time(void);
 
-
 void init_time()
 {
-  timeClient.begin(); // init NTP
+  timeClient.begin();          // init NTP
   timeClient.setTimeOffset(0); // 0= GMT, 3600 = GMT+1
 }
- 
+
 /* 
  *  Implementações das funções
  */
-void setup() 
+void setup()
 {
-    init_serial();
-    init_wifi();
-    init_mqtt();
-    init_time();
-    dht.begin();
-    pinMode(2, OUTPUT);//Pino
-    pinMode(PIN_LUMINOSIDADE,INPUT);
+  init_serial();
+  init_wifi();
+  init_mqtt();
+  init_time();
+  dht.begin();
+  pinMode(2, OUTPUT); //Pino
+  pinMode(PIN_LUMINOSIDADE, INPUT);
 }
 
-
 //Mandar mensagem
-void send_payload(String Umidade,String Temperatura, String Luminosidade, String Presenca)
+void send_payload(String Umidade, String Temperatura, String Luminosidade, bool Presenca)
 {
 
-  while(!timeClient.update()) {
+  while (!timeClient.update())
+  {
     timeClient.forceUpdate();
   }
-  
+
   long unsigned int timestamp = timeClient.getEpochTime();
   StaticJsonDocument<256> doc;
 
@@ -154,16 +160,16 @@ void send_payload(String Umidade,String Temperatura, String Luminosidade, String
   doc["timestamp"] = timestamp;
 
   //Add an object
-  JsonObject sensor = doc.createNestedObject("sensor") ;
-  sensor["temperatura"] = Temperatura+"°C";
-  sensor["umidade"]= Umidade+"%";
+  JsonObject sensor = doc.createNestedObject("sensor");
+  sensor["temperatura"] = Temperatura + "°C";
+  sensor["umidade"] = Umidade + "%";
   sensor["proximidade"] = Luminosidade;
-  sensor["presenca"] = Presenca;
-    
-//  //Add an array.
-//  JsonArray values = doc.createNestedArray("data");
-//  values.add(48.756080);
-//  values.add(2.302038);
+  sensor["presenca"] = String(Presenca);
+
+  //  //Add an array.
+  //  JsonArray values = doc.createNestedArray("data");
+  //  values.add(48.756080);
+  //  values.add(2.302038);
 
   Serial.print(F("Sending: "));
   serializeJson(doc, Serial);
@@ -174,32 +180,28 @@ void send_payload(String Umidade,String Temperatura, String Luminosidade, String
   MQTT.publish(TOPICO_PUBLISH, out);
 }
 
-
-  
 /* Função: inicializa comunicação serial com baudrate 115200 (para fins de monitorar no terminal serial 
            o que está acontecendo.
   Parâmetros: nenhum
   Retorno: nenhum
 */
-void init_serial() 
+void init_serial()
 {
-    Serial.begin(115200);
+  Serial.begin(115200);
 }
 
-
- 
 /* Função: inicializa e conecta-se na rede WI-FI desejada
  * Parâmetros: nenhum
  * Retorno: nenhum
  */
-void init_wifi(void) 
+void init_wifi(void)
 {
-    delay(10);
-    Serial.println("------Conexao WI-FI------");
-    Serial.print("Conectando-se na rede: ");
-    Serial.println(SSID);
-    Serial.println("Aguarde");
-    reconnect_wifi();
+  delay(10);
+  Serial.println("------Conexao WI-FI------");
+  Serial.print("Conectando-se na rede: ");
+  Serial.println(SSID);
+  Serial.println("Aguarde");
+  reconnect_wifi();
 }
 
 /* Função: inicializa parâmetros de conexão MQTT(endereço do  
@@ -207,67 +209,65 @@ void init_wifi(void)
  * Parâmetros: nenhum
  * Retorno: nenhum
  */
-void init_mqtt(void) 
+void init_mqtt(void)
 {
-    /* informa a qual broker e porta deve ser conectado */
-    MQTT.setServer(BROKER_MQTT, BROKER_PORT); 
-    /* atribui função de callback (função chamada quando qualquer informação do 
-    tópico subescrito chega) */         
+  /* informa a qual broker e porta deve ser conectado */
+  MQTT.setServer(BROKER_MQTT, BROKER_PORT);
+  /* atribui função de callback (função chamada quando qualquer informação do 
+    tópico subescrito chega) */
 }
 
-  
 /* Função: reconecta-se ao broker MQTT (caso ainda não esteja conectado ou em caso de a conexão cair)
  *          em caso de sucesso na conexão ou reconexão, o subscribe dos tópicos é refeito.
  * Parâmetros: nenhum
  * Retorno: nenhum
  */
-void reconnect_mqtt(void) 
+void reconnect_mqtt(void)
 {
-    while (!MQTT.connected()) 
+  while (!MQTT.connected())
+  {
+    Serial.print("* Tentando se conectar ao Broker MQTT: ");
+    Serial.println(BROKER_MQTT);
+    if (MQTT.connect(ID_MQTT))
     {
-        Serial.print("* Tentando se conectar ao Broker MQTT: ");
-        Serial.println(BROKER_MQTT);
-        if (MQTT.connect(ID_MQTT)) 
-        {
-            Serial.println("Conectado com sucesso ao broker MQTT!");
-            MQTT.subscribe(TOPICO_SUBSCRIBE); 
-        } 
-        else
-        {
-            Serial.println("Falha ao reconectar no broker.");
-            Serial.println("Havera nova tentatica de conexao em 2s");
-            delay(2000);
-        }
+      Serial.println("Conectado com sucesso ao broker MQTT!");
+      MQTT.subscribe(TOPICO_SUBSCRIBE);
     }
+    else
+    {
+      Serial.println("Falha ao reconectar no broker.");
+      Serial.println("Havera nova tentatica de conexao em 2s");
+      delay(2000);
+    }
+  }
 }
-  
+
 /* Função: reconecta-se ao WiFi
  * Parâmetros: nenhum
  * Retorno: nenhum
 */
-void reconnect_wifi() 
+void reconnect_wifi()
 {
-    /* se já está conectado a rede WI-FI, nada é feito. 
+  /* se já está conectado a rede WI-FI, nada é feito. 
        Caso contrário, são efetuadas tentativas de conexão */
-    if (WiFi.status() == WL_CONNECTED)
-        return;
-         
-    WiFi.begin(SSID, PASSWORD);
-     
-    while (WiFi.status() != WL_CONNECTED) 
-    {
-        delay(100);
-        Serial.print(".");
-    }
-   
-    Serial.println();
-    Serial.print("Conectado com sucesso na rede ");
-    Serial.print(SSID);
-    Serial.println("IP obtido: ");
-    Serial.println(WiFi.localIP());
+  if (WiFi.status() == WL_CONNECTED)
+    return;
+
+  WiFi.begin(SSID, PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.print("Conectado com sucesso na rede ");
+  Serial.print(SSID);
+  Serial.println("IP obtido: ");
+  Serial.println(WiFi.localIP());
 }
 
- 
 /* Função: verifica o estado das conexões WiFI e ao broker MQTT. 
  *         Em caso de desconexão (qualquer uma das duas), a conexão
  *         é refeita.
@@ -276,29 +276,40 @@ void reconnect_wifi()
  */
 void verifica_conexoes_wifi_mqtt(void)
 {
-    /* se não há conexão com o WiFI, a conexão é refeita */
-    reconnect_wifi(); 
-    /* se não há conexão com o Broker, a conexão é refeita */
-    if (!MQTT.connected()) 
-        reconnect_mqtt(); 
-} 
- 
+  /* se não há conexão com o WiFI, a conexão é refeita */
+  reconnect_wifi();
+  /* se não há conexão com o Broker, a conexão é refeita */
+  if (!MQTT.connected())
+    reconnect_mqtt();
+}
+
 /* programa principal */
-void loop() 
-{   
-    String Umidade = readDHTHumidity();
-    String Temperatura = readDHTTemperature();
-    String Luminosidade = readLDRLuminosidade();
-    String Presenca = readPIRPresenca();
-    /* garante funcionamento das conexões WiFi e ao broker MQTT */
-    verifica_conexoes_wifi_mqtt();
-    
-    send_payload(Umidade,Temperatura,Luminosidade,Presenca);
- 
-    /* keep-alive da comunicação com broker MQTT */    
-    MQTT.loop();
-    
-    /* Agurda 1 segundo para próximo envio */
-//    delay(60000);   
-      delay(15000);
+void loop()
+{
+  /* garante funcionamento das conexões WiFi e ao broker MQTT */
+  verifica_conexoes_wifi_mqtt();
+
+  /* keep-alive da comunicação com broker MQTT */
+  MQTT.loop();
+
+  String Umidade = readDHTHumidity();
+  String Temperatura = readDHTTemperature();
+  String Luminosidade = readLDRLuminosidade();
+  bool Presenca = readPIRPresenca();
+  unsigned long currentMillis = millis();
+  if ((Presenca && !wasPresence) || (currentMillis - previousMillis >= interval))
+  {
+    Serial.println("Send payload");
+    send_payload(Umidade, Temperatura, Luminosidade, Presenca);
+    previousMillis = currentMillis;
+    if (Presenca)
+    {
+      Serial.println("Presence detected");
+      wasPresence = true;
+    }
+    else
+    {
+      wasPresence = false;
+    }
+  }
 }

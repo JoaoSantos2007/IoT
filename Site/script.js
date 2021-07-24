3d9c7ed038290508e77af4717312bc709d3840d4
João Pedro Tomaz dos Santos <80163527+JoaoSantos2007@users.noreply.github.com>
2021-07-24 13:48:39 -0300

    update

--- Site/script.js
+++ Site/script.js
@@ -334,10 +334,9 @@ function tipo_air(dados, key, modificar) {
   var nome = dados.name
   var lugar = dados.location
   section.innerHTML += `<div>`
-  section.innerHTML += `<img src="/Site/images/snow.png" alt="AC
-  ">`
-  section.innerHTML += `<img id="edit" src="/Site/images/edit.png" alt="edit" onclick="">`
-  section.innerHTML += `<img id="delete" src="/Site/images/delete.png" alt="delete" onclick="">`
+  section.innerHTML += `<img src="/Site/images/snow.png" alt="AC">`
+  section.innerHTML += `<img id="edit" src="/Site/images/edit.png" alt="edit" onclick="editar_registro('${key}')">`
+  section.innerHTML += `<img id="delete" src="/Site/images/delete.png" alt="delete" onclick="deletar('${key}')">`
   section.innerHTML += `</div>`
   section.innerHTML += `<p>`
   section.innerHTML += `<strong>Nome: </strong>${nome}`

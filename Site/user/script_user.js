f1a4db9287914171f033c90c93ed4a4fe9daf4b4
João Pedro Tomaz dos Santos <80163527+JoaoSantos2007@users.noreply.github.com>
2021-11-05 23:09:46 -0300

    update

--- Site/user/script_user.js
+++ Site/user/script_user.js
@@ -45,31 +45,98 @@ function login() {
 function carregar_dados_usuario() {
     var div_name = window.document.getElementById("div-name")
     var div_email = window.document.getElementById("div-email")
-    var div_foto = window.document.getElementById("div-foto")
-    var div_email_verif = window.document.getElementById("div-email_verif")
+    var div_foto = window.document.getElementById("img-user")
 
     firebase.auth().onAuthStateChanged((user) => {
         const displayName = user.displayName;
         const email = user.email;
         const photoURL = user.photoURL;
         const emailVerified = user.emailVerified;
-        console.log(email)
-        console.log("OK")
         const uid = user.uid;
 
         div_name.innerHTML += `${displayName}`
         div_email.innerHTML += `${email}`
-        div_foto.innerHTML += `${photoURL}`
-        div_email_verif.innerHTML += `${emailVerified}`
+        if(emailVerified != true){
+            var btn_verifEmail = window.document.createElement("input")
+            btn_verifEmail.setAttribute("type","button")
+            btn_verifEmail.setAttribute("value","Verif Email")
+            btn_verifEmail.setAttribute("onclick","verif_email()")
+            btn_verifEmail.setAttribute("id","btn_verif_email")
+            var section = window.document.getElementById("section_user")
+            section.appendChild(btn_verifEmail)
+        }
+        div_foto.setAttribute("src",String(photoURL))
     });
 }
 
 function desconectar() {
-    firebase.auth().signOut().then(() => {
-        // Sign-out successful.
-    }).catch((error) => {
-        console.log('OK')
-        // An error happened.
+    var res_sair = window.confirm("Você deseja encerrar a sessão?")
+    if (res_sair == true) {
+        firebase.auth().signOut().then(() => {
+            // Sign-out successful.
+        }).catch((error) => {
+            window.alert("Erro ao sair")
+            // An error happened.
+        });
+    }
+}
+
+function delete_user() {
+    var res_delete_user = window.confirm("Você deseja excluir essa conta?")
+    if (res_delete_user) {
+        firebase.auth().onAuthStateChanged((user) => {
+            user.delete().then(() => {
+                // User deleted.
+            }).catch((error) => {
+                window.alert("Ocorreu um erro ao excluir")
+            });
+        });
+    }
+
+}
+
+function update_user_image(){
+    firebase.auth().onAuthStateChanged((user) => {
+        const photoURL = user.photoURL;
+        var image = window.document.getElementById("image-user")
+        var url = window.document.getElementById("url-image")
+        if(photoURL == null){
+            image.setAttribute("src","/Site/images/user.ico")
+        }else{
+            image.setAttribute("src",String(photoURL))
+            url.setAttribute("value",String(photoURL))
+        }
     });
 }
 
+function cancel_image(){
+    window.location.href = ("/Site/index.html")
+}
+
+function change_image(){
+    var url = String(window.document.getElementById("url-image").value)
+    console.log(url)
+    firebase.auth().onAuthStateChanged((user) => {
+        user.updateProfile({
+            photoURL: url
+          }).then(() => {
+            setInterval(function () {
+                window.location.href = ("/Site/index.html")
+            }, 1200)
+          }).catch((error) => {
+            window.alert("Ocorreu um erro ao atualizar sua foto de perfil")
+          }); 
+    });
+}
+
+function verif_email(){
+    firebase.auth().onAuthStateChanged((user) => {
+        user.sendEmailVerification().then(() => {
+          window.alert("Verificação de email enviada!")
+        });
+    });
+}
+
+function edit_displayName(){
+    
+}
